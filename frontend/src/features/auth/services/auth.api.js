029e991e60234fb085ca96c38c10523d713c2fa4
Amit Singh <amit81212609@gmail.com>
2026-09-07 14:18:44 +0530

    add backend url

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://skillbridge-ai-o8iu.onrender.com",
   withCredentials: true,
 });
 

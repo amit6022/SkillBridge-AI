63d34a798f7d82958f38bee53c983a5c8893a97e
Amit Singh <amit81212609@gmail.com>
2026-09-07 14:32:59 +0530

    add frontend url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://skillbridge-ai-1-fuqf.onrender.com",
     credentials: true, //----->> because we handle data from cookies
   }),
 );

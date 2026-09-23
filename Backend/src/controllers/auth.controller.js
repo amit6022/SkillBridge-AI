8624180a36a4cbdacbee9074251a6100881f471a
Amit Singh <amit81212609@gmail.com>
2026-09-23 19:04:29 +0530

    Enhance cookie security for token management
    Added cookie options for enhanced security during login and logout.

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -88,7 +88,12 @@ async function loginUserController(req, res) {
     { expiresIn: "1d" },
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  });
+  
   res.status(200).json({
     message: "User loggedIn successfully.",
     user: {
@@ -112,7 +117,11 @@ async function logoutUserController(req, res) {
     tokenBlackListModel.create({ token });
   }
 
-  res.clearCookie("token");
+  res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+});
 
   res.status(200).json({
     message: "User logged out successfully",

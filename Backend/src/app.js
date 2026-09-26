334ecc7849cb15be25711db6f985a4a130407ffa
Amit Singh <amit81212609@gmail.com>
2026-09-26 19:28:01 +0530

    update CORS origin and stop tracking frontend .env

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,8 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    // origin: "https://skillbridge-ai-frontend.netlify.app",
-    origin: "http://localhost:5173",
+    origin: "https://skillbridge-ai-frontend.netlify.app",
     credentials: true, //----->> because we handle data from cookies
   }),
 );
